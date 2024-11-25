df7d996150b3a9a6872cc0ddd1c398869fb807e4
alodlabs <alodlabs@outlook.com>
2024-11-25 18:16:51 +0300

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,17 @@
+from . import db
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    stock = db.Column(db.Integer, nullable=False)
+    image_url = db.Column(db.String(200))
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    customer_name = db.Column(db.String(100), nullable=False)
+    customer_email = db.Column(db.String(100), nullable=False)
+    products = db.Column(db.Text, nullable=False)  # JSON string of product IDs
+    total_price = db.Column(db.Float, nullable=False)
+    status = db.Column(db.String(50), default='Pending')
