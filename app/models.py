a788440c23add7e1eda59cbc53ebedb3fc74cb24
alodlabs <alodlabs@outlook.com>
2024-11-26 01:10:05 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -6,12 +6,4 @@ class Product(db.Model):
     description = db.Column(db.Text, nullable=False)
     price = db.Column(db.Float, nullable=False)
     stock = db.Column(db.Integer, nullable=False)
-    image_url = db.Column(db.String(200))
-
-class Order(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    customer_name = db.Column(db.String(100), nullable=False)
-    customer_email = db.Column(db.String(100), nullable=False)
-    products = db.Column(db.Text, nullable=False)  # JSON string of product IDs
-    total_price = db.Column(db.Float, nullable=False)
-    status = db.Column(db.String(50), default='Pending')
+    image_url = db.Column(db.String(200), default="/static/images/placeholder.png")
